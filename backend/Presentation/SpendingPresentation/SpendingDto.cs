67bfa466d0be8640c37f2bc8fda02d207ec867d4
Miki <vitorhugosnp@gmail.com>
2024-07-31 22:35:17 -0400

    Dto Fix(Centralized all of Them inside .cs file)

--- backend/Presentation/SpendingPresentation/SpendingDto.cs
+++ backend/Presentation/SpendingPresentation/SpendingDto.cs
@@ -7,3 +7,16 @@ public class SpendingDto
     [Required]
     public decimal ValueSpended { get; set; }
 }
+public class CreateSpendingDto
+{
+    public int Id { get; set; }
+    public decimal ValueSpended { get; set; }
+    public DateTime SpendingDate { get; set; } = DateTime.Now;
+    public string? UserId { get; set; }
+}
+
+public class UpdateSpendingDto
+{
+    public int Id { get; set; }
+    public decimal ValueSpended { get; set; }
+}
--- backend/Presentation/UserPresentation/UserDto.cs
+++ backend/Presentation/UserPresentation/UserDto.cs
@@ -13,3 +13,23 @@ public class UserDto
     public string Token { get; set; } = null!;
     public List<CreateSpendingDto> Spendings { get; set; }
 }
+
+public class RegisterUserDto
+{
+    [Required]
+    public string Username { get; set; } = null!;
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+    [Required]
+    public string Password { get; set; } = null!;
+    public decimal Money { get; set; }
+}
+
+public class LoginUserDto
+{
+    [Required]
+    public string Username { get; set; } = null!;
+    [Required]
+    public string Password { get; set; } = null!;
+}
