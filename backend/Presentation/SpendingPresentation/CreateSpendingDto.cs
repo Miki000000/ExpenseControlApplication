2b9748bda2b21fedc51c3458fc4ca856dd32d69c
Miki <vitorhugosnp@gmail.com>
2024-07-31 22:37:23 -0400

    Deleted .cs Files due to the centralization of DTOs

--- backend/Presentation/SpendingPresentation/CreateSpendingDto.cs
+++
@@ -1,9 +0,0 @@
-namespace ExpenseControlApplication.Presentation.SpendingPresentation;
-
-public class CreateSpendingDto
-{
-    public int Id { get; set; }
-    public decimal ValueSpended { get; set; }
-    public DateTime SpendingDate { get; set; } = DateTime.Now;
-    public string? UserId { get; set; }
-}
