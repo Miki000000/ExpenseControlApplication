a662995892385993b0ad0f504dac07aeada71d2a
Miki <vitorhugosnp@gmail.com>
2024-08-01 21:24:29 -0400

    Injected tokenService in the class service

--- backend/Business/Services/UserServices.cs
+++ backend/Business/Services/UserServices.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ExpenseControlApplication.Business.Services;
 
-public class UserServices(IUserRepository userRepo) 
+public class UserServices(IUserRepository userRepo, ITokenService tokenService) 
     : IUserServices
 {
     public async Task<UserDto> RegisterUser(RegisterUserDto userDto)
