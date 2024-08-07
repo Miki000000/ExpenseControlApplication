de20501d74586aca2a88188920dce53f0eccd0c2
Miki <vitorhugosnp@gmail.com>
2024-08-06 21:53:03 -0400

    Minor change

--- backend/Utils/Exceptions/InvalidEntryException.cs
+++ backend/Utils/Exceptions/InvalidEntryException.cs
@@ -1,4 +1,4 @@
 namespace ExpenseControlApplication.Utils.Exceptions;
 
 public class InvalidEntryException(string property) 
-    : Exception($"Invalid {property}!");
+    : Exception($"Invalid: {property}!");
